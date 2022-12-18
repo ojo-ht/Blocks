c8ea76343f1ba7bd0107f13f0891a31f0e5d1808
Gaia Sound <gaia-sound@protonmail.com>
2022-12-18 11:56:06 -0700

    add airy plots and stuff

--- chapter-2-special-functions/index.md
+++
@@ -1,5 +0,0 @@
-## [The Airy Function](https://ojo-ht.github.io/Blocks/chapter-2-special-functions/airy-function/index.md)
-
-## The Sinc Function
-
-## Lorentzian
--- source/airy-function/airy-function.py
+++ source/airy-function/airy-function.py
@@ -4,17 +4,21 @@ from scipy import special
 
 # Real
 
-x = np.linspace(-100, 10, 20000)
+x = np.linspace(-10, 6, 1000)
 ai, aip, bi, bip = special.airy(x)
 
-fig, ax = plt.subplots()
+fig, axes = plt.subplots(2, 1, sharex=True) # 0 top, 1 bottom
 
-ax.plot(x, ai, 'r', label='Ai(x)')
-ax.plot(x, bi, 'b--', label='Bi(x)')
+axes[0].plot(x, ai, 'r', label='Ai(x)')
+axes[1].plot(x, bi, 'b--', label='Bi(x)')
 
-ax.set_ylim(-0.5, 1.0)
-ax.legend(loc='upper left')
+for axis in axes:
+    axis.set_ylim(-0.5, 1.0)
+    axis.set_yticks([])
+    axis.legend(loc='upper right')
 
+axes[1].set_xlim(x.min(), x.max())
+    
 plt.show()
 
 # Complex
