586bfe2796a830dc6f0e6c36e20492ba34fffd7a
Gaia Sound <gaia-sound@protonmail.com>
2022-12-19 07:26:13 -0700

    implement calculations of airy function in the complex domain.
    add images that show Im(Bi(z))

--- source/airy-function/airy-function.py
+++ source/airy-function/airy-function.py
@@ -19,10 +19,55 @@ for axis in axes:
     axis.axis('off')
 
 axes[1].set_xlim(x.min(), x.max())
-plt.show()
+
 
 # Complex
 
 real = np.linspace(-10,10,100)
 imag = real
 
+nx, ny = (301, 301)
+
+x = np.linspace(-7, 7, nx)
+y = np.linspace(-7, 7, ny)
+
+xv, yv = np.meshgrid(x, y)
+
+complex_grid = np.zeros(xv.shape, dtype=np.complex_)
+
+complex_grid.real = xv
+complex_grid.imag = yv
+
+ai, aip, bi, bip = special.airy(complex_grid)
+
+bi_imag = np.clip(bi.imag, -2.5, 2.5)
+
+fig3, ax3 = plt.subplots()
+ax3.contourf(xv, yv, bi_imag, levels=20, cmap='magma')
+ax3.contour(xv, yv, bi_imag, levels=20, colors='k',  linestyles='solid')
+ax3.axis('off')
+ax3.set_position([0, 0, 1, 1])
+
+
+
+fig4 = plt.figure()
+ax4 = fig4.add_subplot(projection='3d')
+ax4.plot_surface(xv, yv, bi_imag, cmap='magma', linewidth=0, rstride=5, cstride=5)
+ax4.set_position([0, 0, 1, 1])
+ax4.xaxis.set_ticklabels([])
+ax4.yaxis.set_ticklabels([])
+ax4.zaxis.set_ticklabels([])
+
+for line in ax4.xaxis.get_ticklines():
+    line.set_visible(False)
+for line in ax4.yaxis.get_ticklines():
+    line.set_visible(False)
+for line in ax4.zaxis.get_ticklines():
+    line.set_visible(False)
+
+ax4.view_init(elev=49, azim=-116, roll=0)
+
+fig3.savefig('/home/josep/Documents/Blocks/docs/chapter-2-special-functions/airy-function/images/airy-contour-highdpi.png', format='png', dpi=1200)
+fig4.savefig('/home/josep/Documents/Blocks/docs/chapter-2-special-functions/airy-function/images/airy-3dsurf-highdpi.png', format='png', dpi=1200)
+
+plt.show()
